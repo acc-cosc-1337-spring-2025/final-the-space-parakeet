bc82e3fa95ff2539e3bbf086dcb56845c618e316
the-space-parakeet <166778435+the-space-parakeet@users.noreply.github.com>
2025-05-13 16:36:45 +0000

    Question 2

--- src/roll.cpp
+++ src/roll.cpp
@@ -1,2 +1,22 @@
+#include "roll.h"
 
+/*------------------------------------------------------------------------------
+    Roll Public Member Functions
+------------------------------------------------------------------------------*/
+
+Roll::Roll(Die& die1, Die& die2):
+    die1(die1), die2(die2) {
+}
+
+// roll dice
+// - roll die1 and die2
+// - save the sum to rolled_value
+void Roll::roll_dice() {
+    rolled_value = die1.roll() + die2.roll();
+}
+
+// return rolled value
+int Roll::roll_value () const {
+    return rolled_value;
+}
 
--- test/craps_test.cpp
+++ test/craps_test.cpp
@@ -10,8 +10,25 @@ TEST_CASE("die rolls values in range [1, 6]") {
 	int rolled;
 	for (int i = 0; i < 10; ++i) {
 		rolled = die.roll();
-		cout << rolled;
+		cout << "\trolled:\t" << rolled << "\n";
 		REQUIRE(1 <= rolled && rolled <= 6);
+	}
+}
+
+TEST_CASE("two dice roll values in range [2, 12]") {
+	Die die1 {};
+	Die die2 {};
+	int rolled1;
+	int rolled2;
+	int rolled_sum;
+	for (int i = 0; i < 10; ++i) {
+		rolled1 = die1.roll();
+		rolled2 = die2.roll();
+		rolled_sum = rolled1 + rolled2;
+		cout << "\trolled1:\t" << rolled1 << "\n";
+		cout << "\trolled2:\t" << rolled2 << "\n";
+		cout << "\trolled_sum:\t" << rolled_sum << "\n";
+		REQUIRE(2 <= rolled_sum && rolled_sum <= 12);
 	}
 }
 
