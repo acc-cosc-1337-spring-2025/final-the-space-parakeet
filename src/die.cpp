e40b3da0fe9813985ea5af21bd9d8a37ac3a36d0
the-space-parakeet <166778435+the-space-parakeet@users.noreply.github.com>
2025-05-13 15:03:08 +0000

    Question 1

--- src/die.cpp
+++ src/die.cpp
@@ -1,1 +1,13 @@
-//
+#include "die.h"
+
+/*------------------------------------------------------------------------------
+    Die Public Member Functions
+------------------------------------------------------------------------------*/
+
+// roll die
+// - generate a random integer in range [1, sides]
+// - integers will have a uniform distribution
+int Die::roll () {
+    std::uniform_int_distribution<int> distribution {1, sides};
+    return distribution(generator);
+}
--- src/die.h
+++ src/die.h
@@ -1,1 +1,19 @@
-//
+#pragma once
+
+#include <ctime>
+#include <random>
+
+/*------------------------------------------------------------------------------
+    Die Class
+------------------------------------------------------------------------------*/
+
+class Die {
+    public:
+        int roll ();
+    private:
+        int sides {6};
+        // die is seeded with source of entropy from device on initialisation
+        // die uses 64-bit mersenne twister engine and a uniform distribution
+        std::random_device seed;
+        std::mt19937_64 generator {seed()};
+};
