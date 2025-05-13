9789889698abfaf82746d1a254fb013466651e6f
the-space-parakeet <166778435+the-space-parakeet@users.noreply.github.com>
2025-05-13 15:49:02 +0000

    Question 1

--- src/die.cpp
+++ src/die.cpp
@@ -5,7 +5,7 @@
 ------------------------------------------------------------------------------*/
 
 // roll die
-// - generate a random integer in range [1, sides]
+// - return a random integer in range [1, sides]
 // - integers will have a uniform distribution
 int Die::roll () {
     std::uniform_int_distribution<int> distribution {1, sides};
--- test/craps_test.cpp
+++ test/craps_test.cpp
@@ -1,6 +1,17 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include <iostream>
+#include "die.h"
 
-TEST_CASE("Verify Test Configuration", "verification") {
-	REQUIRE(true == true);
+using std::cout;
+
+TEST_CASE("die rolls values in range [1, 6]") {
+	Die die {};
+	int rolled;
+	for (int i = 0; i < 10; ++i) {
+		rolled = die.roll();
+		cout << rolled;
+		REQUIRE(1 <= rolled && rolled <= 6);
+	}
 }
+
